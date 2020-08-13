657ef65cdb9c52adfb3d71c926523eda77d50bac
Simon Zeng <zeng.simonl@gmail.com>
2020-08-13 18:18:29 -0400

    Started adding the navbar functionality

---
+++ client/components/Pick-Company.js
@@ -0,0 +1,5 @@
+// Just has a form, that asks for the company name
+
+// It will enter that company name into the news api
+
+// Redirects you to the component of the company that you entered, and the articles on it
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,56 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
-
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
+export default class Navbar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      company: ''
     }
   }
-}
 
-export default connect(mapState, mapDispatch)(Navbar)
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
 
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="search-box">
+          <input
+            className="search-txt"
+            type="text"
+            name=""
+            value=""
+            placeholder="Company Name"
+          />
+        </div>
+      </div>
+    )
+  }
 }
