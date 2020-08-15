a2df6848916c26531623fe3b16526adb13d5eefc
Jake <jakejaekwak@gmail.com>
2020-08-15 17:44:03 -0400

    updates

--- client/components/CompanyInfo.js
+++ client/components/CompanyInfo.js
@@ -14,6 +14,7 @@ class CompanyInfo extends React.Component {
   render() {
     let articles = this.props.articles || []
     console.log('this is articles', articles)
+    console.log('this is props', this.props)
 
     return (
       <div>
